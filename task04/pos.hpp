72a8e1ec16f8a4e4702b6e1b1528e3749b887dd2
Evan Le Bihan <evan.lebihan@ecole-89.com>
2023-10-02 17:25:18 +0200

    chap3

---
+++ task04/pos.hpp
@@ -0,0 +1,33 @@
+#ifndef POS_HPP_
+#define POS_HPP_
+#include <iostream>
+
+template <typename T>
+struct pos_2d {
+    T x;
+    T y;
+};
+
+template <typename T>
+struct pos_3d {
+    T x;
+    T y;
+    T z;
+};
+
+template<typename T>
+std::ostream &operator<<(std::ostream &os, const pos_2d<T> &rhs)
+{
+    os << "(" << rhs.x << ", " << rhs.y << ")";
+    return os;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const pos_3d<T> &rhs)
+{
+    os << "(" << rhs.x << ", " << rhs.y << ", " << rhs.z << ")";
+    return os;
+}
+
+
+#endif
