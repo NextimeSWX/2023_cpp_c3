4eec259e001648e7ea58d3b44291a441208b1de4
Evan Le Bihan <evan.lebihan@ecole-89.com>
2023-10-02 12:18:30 +0200

    chap3

--- task00/hello.cpp
+++ task00/hello.cpp
@@ -4,10 +4,10 @@
 
 void hello(std::string str)
 {
-    std::cout << "Hello" << str << "!\n";
+    std::cout << "Hello " << str << " !\n";
 }
 
 void hello()
 {
-    std::cout << "Hello strangers !";
+    std::cout << "Hello strangers !\n";
 }
